110c40cd3f43e4b4eea8bbcf5fef5165f35f6d4f
Kim Dill-McFarland <kdillmcfarland@gmail.com>
2021-05-18 20:29:37 -0700

    Add y facet labels to GSEA plot

--- publication/scripts/Fig6.GSEA.R
+++ publication/scripts/Fig6.GSEA.R
@@ -5,50 +5,58 @@ library(limma)
 set.seed(4389)
 
 #### Data ####
-H <- read_csv("results/GSEA_FoldChange/h_GSEA.result.csv")
+H <- read_csv("results/GSEA/h_GSEA.result.csv")
 fdr.cut <- 0.1
 
 #### Subset significant ####
 #list all terms with at least 1 significant treatment contrast
 term.OI.t <- H %>% 
   filter(fgsea.FDR <= fdr.cut & gage.FDR <= fdr.cut) %>% 
-  filter(group %in% c("AntiIL5_none.none_none.2", "AntiIL5_HRV.none_HRV.2",
-                      "EOS.supp_none.none_none.1", "EOS.supp_HRV.none_HRV.1")) %>% 
+  filter(group %in% c("AntiIL5_nonenone_none", "AntiIL5_HRVnone_HRV",
+                      "EOSsupp_nonenone_none", "EOSsupp_HRVnone_HRV")) %>% 
   distinct(pathway) %>% unlist(use.names=FALSE)
 
 #list all terms with at least 1 significant virus contrast 
 term.OI.v <- H %>% 
   filter(fgsea.FDR <= fdr.cut & gage.FDR <= fdr.cut) %>% 
-  filter(group %in% c("none_HRV.none_none.2","AntiIL5_HRV.AntiIL5_none.2",
-                      "none_HRV.none_none.1","EOS.supp_HRV.EOS.supp_none.1")) %>% 
+  filter(group %in% c("none_HRVnone_none","AntiIL5_HRVAntiIL5_none",
+                      "EOSsupp_HRVEOSsupp_none")) %>% 
   distinct(pathway) %>% unlist(use.names=FALSE)
 
 #Find terms with both t and v cutoffs
 term.OI <- intersect(term.OI.t, term.OI.v)
 
+
 #### Combine data and format labels ####
 h.plot.dat <- H %>% 
   filter(pathway %in% term.OI) %>% 
-  mutate(group1 = recode_factor(factor(group),
-                                "AntiIL5_HRV.AntiIL5_none.2"='"+ Anti-IL-5/5R"*alpha',
-                                "none_HRV.none_none.2"='"- Anti-IL-5/5R"*alpha',
-                                "EOS.supp_HRV.EOS.supp_none.1"='"+ EOS sup"',
-                                "none_HRV.none_none.1"='"- EOS sup"',
-                                
-                                "EOS.supp_HRV.none_HRV.1"="+ RV",
-                                "AntiIL5_HRV.none_HRV.2"="+ RV",
-                                "EOS.supp_none.none_none.1"="- RV",
-                                "AntiIL5_none.none_none.2"="- RV"),
+  #Add experi group
+  mutate(experi = c(rep("AntiIL5",nrow(.)/2),rep("EOSsup",nrow(.)/2))) %>% 
+  mutate(group1 = recode(group,
+                                "AntiIL5_HRVAntiIL5_none"='"+ Anti-IL-5/5R"*alpha',
+                                "EOSsupp_HRVEOSsupp_none"='"+ EOS sup"',
+
+                                "EOSsupp_HRVnone_HRV"="+ RV",
+                                "AntiIL5_HRVnone_HRV"="+ RV",
+                                "EOSsupp_nonenone_none"="- RV",
+                                "AntiIL5_nonenone_none"="- RV"),
+         group1 = ifelse(group1 == "none_HRVnone_none" & experi == "AntiIL5",
+                         '"- Anti-IL-5/5R"*alpha',
+                         ifelse(group1 == "none_HRVnone_none" & experi == "EOSsup",
+                                '"- EOS sup"', group1)),
          group2 = recode_factor(factor(group),
-            "none_HRV.none_none.1"='italic("Ex vivo")~"RV-infected vs media"',
-            "none_HRV.none_none.2"='italic("Ex vivo")~"RV-infected vs media"',
-            "EOS.supp_HRV.EOS.supp_none.1"='italic("Ex vivo")~"RV-infected vs media"',
-            "AntiIL5_HRV.AntiIL5_none.2"='italic("Ex vivo")~"RV-infected vs media"',
+            "none_HRVnone_none"='italic("Ex vivo")~"RV-infected vs media"',
+            "EOSsupp_HRVEOSsupp_none"='italic("Ex vivo")~"RV-infected vs media"',
+            "AntiIL5_HRVAntiIL5_none"='italic("Ex vivo")~"RV-infected vs media"',
                                 
-            "EOS.supp_HRV.none_HRV.1"='italic("Ex vivo")~"EOS supernatant vs none"',
-            "AntiIL5_HRV.none_HRV.2"='italic("In vivo")~"Anti-IL-5/5R"*alpha~"vs none"',
-            "EOS.supp_none.none_none.1"='italic("Ex vivo")~"EOS supernatant vs none"',
-            "AntiIL5_none.none_none.2"='italic("In vivo")~"Anti-IL-5/5R"*alpha~"vs none"')) %>% 
+            "EOSsupp_HRVnone_HRV"='italic("Ex vivo")~"EOS supernatant vs none"',
+            "AntiIL5_HRVnone_HRV"='italic("In vivo")~"Anti-IL-5/5R"*alpha~"vs none"',
+            "EOSsupp_nonenone_none"='italic("Ex vivo")~"EOS supernatant vs none"',
+            "AntiIL5_nonenone_none"='italic("In vivo")~"Anti-IL-5/5R"*alpha~"vs none"')) %>% 
+  #order factors
+  mutate(group1 = factor(group1, levels=c('"+ Anti-IL-5/5R"*alpha','"- Anti-IL-5/5R"*alpha',
+                                          '"+ EOS sup"','"- EOS sup"', 
+                                          '+ RV', '- RV'))) %>% 
   mutate(Significance = ifelse(fgsea.FDR <= 0.05 & 
                                  gage.FDR <= 0.05 & 
                                  fgsea.FC == gage.FC, 
@@ -84,7 +92,6 @@ h.plot1 <- h.plot.dat %>%
              labeller = labeller(group2=label_parsed)) +
   theme_bw() +
   theme(legend.position = "none") +
-  theme(strip.text.y = element_blank())+
   theme(panel.grid.major.y = element_blank(),
         panel.grid.minor.y = element_blank(),
         strip.background =element_rect(fill="white")) +
@@ -112,11 +119,10 @@ h.plot2 <- h.plot.dat %>%
   theme(panel.grid.major.y = element_blank(),
         panel.grid.minor.y = element_blank(),
         strip.background =element_rect(fill="white"))
-
 #h.plot2
 
 #### Save ####
-h.plot.all <- plot_grid(h.plot1, h.plot2, ncol=2, rel_widths = c(0.6,1),
+h.plot.all <- plot_grid(h.plot1, h.plot2, ncol=2, rel_widths = c(0.55,1),
                         labels=c("A","B")) 
 #h.plot.all
 ggsave("publication/fig/Fig6.GSEA.pdf", h.plot.all, 
